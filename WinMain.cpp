ad0d08fa194987754c273adc8f1d2b152454d5ba
JonnyBoyZ <47601577+Senzic@users.noreply.github.com>
2023-07-19 19:16:15 +0100

    Revert "More cleanup"
    This reverts commit c03545a96b64c5d2f1472648668aecfccb7dcda8.

--- WinMain.cpp
+++ WinMain.cpp
@@ -4,18 +4,18 @@
 int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
 {
 	WNDCLASSEXW wc = {};
-	wc.cbSize			= sizeof(WNDCLASSEXW);
-	wc.style			= 0;
-	wc.lpfnWndProc		= nullptr;
-	wc.cbClsExtra		= 0;
-	wc.cbWndExtra		= 0;
-	wc.hInstance		= hInstance;
-	wc.hIcon			= 0;
-	wc.hCursor			= 0;
-	wc.hbrBackground	= 0;
-	wc.lpszMenuName		= 0;
-	wc.lpszClassName	= L"Main Window Class";
-	wc.hIconSm			= 0;
+	wc.cbSize = sizeof(WNDCLASSEXW);
+	wc.style = 0;
+	wc.lpfnWndProc = nullptr;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = 0;
+	wc.hCursor = 0;
+	wc.hbrBackground = 0;
+	wc.lpszMenuName = 0;
+	wc.lpszClassName = L"Main Window Class";
+	wc.hIconSm = 0;
 	Window myWindow(wc);
 
 	// Message Loop
