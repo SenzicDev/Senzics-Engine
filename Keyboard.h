75a3874caec517f780c359f7833eea4e711fe81b
JonnyBoyZ <47601577+Senzic@users.noreply.github.com>
2023-07-19 19:16:42 +0100

    asdf

---
+++ Keyboard.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <optional>
+
+class Keyboard
+{
+public:
+	Keyboard() = default;
+	~Keyboard() = default;
+	Keyboard(const Keyboard&) = delete;
+	Keyboard& operator=(const Keyboard&) = delete;
+
+private:
+
+
+};
